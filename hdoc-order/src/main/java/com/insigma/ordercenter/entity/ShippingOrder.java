f05cd1da27125934dc78ff286eea163c3d35f204
hao_liu <liuhao2@bis.com.cn>
2020-07-08 18:03:20 +0800

    基础类修改

---
+++ hdoc-order/src/main/java/com/insigma/ordercenter/entity/ShippingOrder.java
@@ -0,0 +1,85 @@
+package com.insigma.ordercenter.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+
+import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import java.io.Serializable;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 发货单表
+ * </p>
+ *
+ * @author LiuHao
+ * @since 2020-07-08
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("t_shipping_order")
+@ApiModel(value = "ShippingOrder对象", description = "发货单表")
+public class ShippingOrder implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "发货单ID")
+    @TableId(value = "shipping_order_id", type = IdType.ID_WORKER)
+    private Long shippingOrderId;
+
+    @ApiModelProperty(value = "仓库ID ")
+    private Long warehouseId;
+
+    @ApiModelProperty(value = "物流公司ID")
+    private Long expressCompanyId;
+
+    @ApiModelProperty(value = "物流单号")
+    private String expressNo;
+
+    @ApiModelProperty(value = "创建人id")
+    private Long createId;
+
+    @ApiModelProperty(value = "创建时间")
+    private LocalDateTime createTime;
+
+    @ApiModelProperty(value = "修改人id")
+    private Long modifyId;
+
+    @ApiModelProperty(value = "修改时间")
+    private LocalDateTime modifyTime;
+
+    @ApiModelProperty(value = "是否删除（0：未删除，1：已删除）")
+    private Integer isDeleted;
+
+    @ApiModelProperty(value = "状态（0：待出库，1：待取货，2：已发货，3：冻结，4：冻结）")
+    private Integer status;
+
+    @ApiModelProperty(value = "收货人姓名")
+    private String receiveName;
+
+    @ApiModelProperty(value = "联系方式")
+    private String mobilePhone;
+
+    @ApiModelProperty(value = "要求收货时间")
+    private LocalDateTime requestTime;
+
+    @ApiModelProperty(value = "收货人地址")
+    private String address;
+
+    @ApiModelProperty(value = "收货人备注")
+    private String receiveRemark;
+
+    @ApiModelProperty(value = "是否为拆合单（0：否，1：拆单，2：合单）")
+    private Integer isCombined;
+
+
+}
--- hdoc-order/src/main/java/com/insigma/ordercenter/entity/StockOperationLog.java
+++ hdoc-order/src/main/java/com/insigma/ordercenter/entity/StockOperationLog.java
@@ -1,57 +1,53 @@
 package com.insigma.ordercenter.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
-
-import java.time.LocalDateTime;
-
-import com.baomidou.mybatisplus.annotation.TableId;
-
-import java.io.Serializable;
-
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
+    import com.baomidou.mybatisplus.annotation.TableName;
+    import com.baomidou.mybatisplus.annotation.IdType;
+    import java.time.LocalDateTime;
+    import com.baomidou.mybatisplus.annotation.TableId;
+    import java.io.Serializable;
+    import io.swagger.annotations.ApiModel;
+    import io.swagger.annotations.ApiModelProperty;
+    import lombok.Data;
+    import lombok.EqualsAndHashCode;
+    import lombok.experimental.Accessors;
 
 /**
- * <p>
- * 库存记录操作日志表
- * </p>
- *
- * @author LiuHao
- * @since 2020-07-08
- */
-@Data
-@EqualsAndHashCode(callSuper = false)
-@Accessors(chain = true)
-@TableName("t_stock_operation_log")
-@ApiModel(value = "StockOperationLog对象", description = "库存记录操作日志表")
-public class StockOperationLog implements Serializable {
+* <p>
+    * 库存记录操作日志表
+    * </p>
+*
+* @author LiuHao
+* @since 2020-07-08
+*/
+    @Data
+        @EqualsAndHashCode(callSuper = false)
+    @Accessors(chain = true)
+    @TableName("t_stock_operation_log")
+    @ApiModel(value="StockOperationLog对象", description="库存记录操作日志表")
+    public class StockOperationLog implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "库存记录操作日志ID")
-    @TableId(value = "stock_operation_log_id", type = IdType.ID_WORKER)
+            @ApiModelProperty(value = "库存记录操作日志ID")
+            @TableId(value = "stock_operation_log_id", type = IdType.ID_WORKER)
     private Long stockOperationLogId;
 
-    @ApiModelProperty(value = "库存表ID")
-    private Long wharehouseProductRelationId;
+            @ApiModelProperty(value = "库存表ID")
+    private Long warehouseProductRelationId;
 
-    @ApiModelProperty(value = "创建人id")
+            @ApiModelProperty(value = "创建人id")
     private Long createId;
 
-    @ApiModelProperty(value = "创建时间")
+            @ApiModelProperty(value = "创建时间")
     private LocalDateTime createTime;
 
-    @ApiModelProperty(value = "原数量")
+            @ApiModelProperty(value = "原数量")
     private Integer originQuantity;
 
-    @ApiModelProperty(value = "变更后数量")
+            @ApiModelProperty(value = "变更后数量")
     private Integer destinationQuantity;
 
-    @ApiModelProperty(value = "变更数量")
+            @ApiModelProperty(value = "变更数量")
     private Integer changeQuantity;
 
 
