0dda3d0c614adce30acd036e3204b184f00418cf
潘俊财 <wx_37055e73600c423ab3d78c5587455a6d@git.code.tencent.com>
2020-07-21 16:58:36 +0800

    物流接口开发

---
+++ hdoc-order/src/main/java/com/insigma/ordercenter/logistics/best/BestUtil.java
@@ -0,0 +1,12 @@
+package com.insigma.ordercenter.logistics.best;
+
+/**
+ * 百世物流接口
+ *
+ * @author Pan Juncai
+ * @version 1.0
+ * @date 2020/7/21 16:56
+ */
+public class BestUtil {
+
+}
---
+++ hdoc-order/src/main/java/com/insigma/ordercenter/logistics/jd/JDUtil.java
@@ -0,0 +1,12 @@
+package com.insigma.ordercenter.logistics.jd;
+
+/**
+ * 京东物流接口
+ *
+ * @author Pan Juncai
+ * @version 1.0
+ * @date 2020/7/21 16:57
+ */
+public class JDUtil {
+
+}
