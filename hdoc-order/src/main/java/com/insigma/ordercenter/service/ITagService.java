9e3de84167e0541eed532b91433fa585e81aad50
991530643 <991530643@qq.com>
2020-07-24 15:13:13 +0800

    商品模块调整。

---
+++ hdoc-order/src/main/java/com/insigma/ordercenter/service/ITagService.java
@@ -0,0 +1,16 @@
+package com.insigma.ordercenter.service;
+
+import com.insigma.ordercenter.entity.Tag;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 商品标签表 服务类
+ * </p>
+ *
+ * @author LiuHao
+ * @since 2020-07-24
+ */
+public interface ITagService extends IService<Tag> {
+
+}
