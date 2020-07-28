6d709bee8af7dd572be8fb77fb1e5fe91a431de2
hongsifan@bis.com.cn <zxcvbnm>
2020-07-28 10:23:43 +0800

    商品仓库信息

--- hdoc-order/src/main/java/com/insigma/ordercenter/entity/StockOperationLog.java
+++ hdoc-order/src/main/java/com/insigma/ordercenter/entity/StockOperationLog.java
@@ -35,7 +35,7 @@ public class StockOperationLog implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "库存记录操作日志ID")
-    @TableId(value = "stock_operation_log_id", type = IdType.AUTO)
+    @TableId(value = "stock_operation_log_id", type = IdType.ID_WORKER)
     private Long stockOperationLogId;
 
     @ApiModelProperty(value = "库存表ID")
