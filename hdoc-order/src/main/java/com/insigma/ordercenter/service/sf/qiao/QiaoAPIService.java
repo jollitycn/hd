f88e1097e80d19db83755e5575e9b6871a3b1c4a
hongsifan@bis.com.cn <zxcvbnm>
2020-08-04 15:46:17 +0800

    冷链接口

--- hdoc-order/src/main/java/com/insigma/ordercenter/service/sf/qiao/QiaoAPIService.java
+++ hdoc-order/src/main/java/com/insigma/ordercenter/service/sf/qiao/QiaoAPIService.java
@@ -56,45 +56,4 @@ public class QiaoAPIService {
         System.out.println("===返回结果：" + result);
         return JSON.parseObject(result, APIResponse.class);
     }
-
-    public static String query(EspServiceCode testService) throws UnsupportedEncodingException {
-        CallExpressServiceTools client = CallExpressServiceTools.getInstance();
-
-        // set common header
-        Map<String, String> params = new HashMap<String, String>();
-
-        String timeStamp = String.valueOf(System.currentTimeMillis());
-        String msgData = CallExpressServiceTools.packageMsgData(testService);
-
-        params.put("partnerID", CLIENT_CODE);  // 顾客编码 ，对应丰桥上获取的clientCode
-        params.put("requestID", UUID.randomUUID().toString().replace("-", ""));
-        params.put("serviceCode", testService.getCode());// 接口服务码
-        params.put("timestamp", timeStamp);
-        params.put("msgData", msgData);
-        params.put("msgDigest", CallExpressServiceTools.getMsgDigest(msgData, timeStamp, CHECK_WORD));
-
-        // System.out.println(params.get("requestID"));
-        long startTime = System.currentTimeMillis();
-
-        System.out.println("====调用请求：" + params.get("msgData"));
-        String result = HttpClientUtil.post(CALL_URL_BOX, params);
-
-        System.out.println("====调用丰桥的接口服务代码：" + String.valueOf(testService.getCode()) + " 接口耗时：" + String.valueOf(System.currentTimeMillis() - startTime) + "====");
-        System.out.println("===调用地址 ===" + CALL_URL_BOX);
-        System.out.println("===顾客编码 ===" + CLIENT_CODE);
-        System.out.println("===返回结果：" + result);
-        return result;
-    }
-
-    public static void main(String[] args) throws UnsupportedEncodingException {
-          	EspServiceCode testService = EspServiceCode.EXP_RECE_CREATE_ORDER; //下订单 //ok
-      //  EspServiceCode testService = EspServiceCode.EXP_RECE_SEARCH_ORDER_RESP; //查订单 //ok
-        //   EspServiceCode testService = EspServiceCode.EXP_RECE_UPDATE_ORDER;//订单取消
-        //  	EspServiceCode testService = EspServiceCode.EXP_RECE_FILTER_ORDER_BSP;//订单筛选
-        // EspServiceCode testService = EspServiceCode.EXP_RECE_SEARCH_ROUTES;//查路由 ok
-        //	EspServiceCode testService = EspServiceCode.EXP_RECE_GET_SUB_MAILNO;//子单号
-        //	EspServiceCode testService = EspServiceCode.EXP_RECE_QUERY_SFWAYBILL;//查运费
-
-        query(testService);
-    }
 }
