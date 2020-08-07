896134b0418c2abddd30bd74f4e8e46e1f795359
hongsifan@bis.com.cn <zxcvbnm>
2020-08-07 15:45:25 +0800

    edb

--- hdoc-order/src/main/java/com/insigma/ordercenter/OrderApplication.java
+++ hdoc-order/src/main/java/com/insigma/ordercenter/OrderApplication.java
@@ -20,8 +20,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class OrderApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(OrderApplication.class,args);
-
     }
 }
---
+++ hdoc-order/src/main/java/com/insigma/ordercenter/logistics/hdjf/edb/EdbNotifyTrade.java
@@ -0,0 +1,31 @@
+package com.insigma.ordercenter.logistics.hdjf.edb;
+
+import com.insigma.ordercenter.entity.vo.BaseVO;
+import lombok.Data;
+
+@Data
+public class EdbNotifyTrade extends BaseVO {
+    /**
+     * bizOrderId : WZ201804270001
+     * bizOrderPushType : 部分退款
+     */
+
+    private String bizOrderId;
+    private String bizOrderPushType;
+
+    public String getBizOrderId() {
+        return bizOrderId;
+    }
+
+    public void setBizOrderId(String bizOrderId) {
+        this.bizOrderId = bizOrderId;
+    }
+
+    public String getBizOrderPushType() {
+        return bizOrderPushType;
+    }
+
+    public void setBizOrderPushType(String bizOrderPushType) {
+        this.bizOrderPushType = bizOrderPushType;
+    }
+}
